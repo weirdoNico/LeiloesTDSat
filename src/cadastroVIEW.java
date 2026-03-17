d9915e6dcb1c34d5dd9e47f06b06bf694f330c60
Nicholas Dela Pace <nicholasdelapace@gmail.com>
2026-03-17 20:07:26 -0300

    Define cadastroVIEW como tela inicial

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -140,6 +140,7 @@ public class cadastroVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_cadastroNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
+
         ProdutosDTO produto = new ProdutosDTO();
         String nome = cadastroNome.getText();
         String valor = cadastroValor.getText();
