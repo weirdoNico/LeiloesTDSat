334094affea32123a700ab1c9ccf07d9d17e7cc7
Nicholas Dela Pace <nicholasdelapace@gmail.com>
2026-03-17 20:09:10 -0300

    Funcionalidade de salvar

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,6 +13,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 
 public class ProdutosDAO {
@@ -22,12 +23,20 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    public int cadastrarProduto (ProdutosDTO produto){
+        int status;
+
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES(?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            status = prep.executeUpdate();
+            return status;
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return ex.getErrorCode();
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
